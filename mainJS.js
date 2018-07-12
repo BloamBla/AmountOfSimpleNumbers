c3edfb5a3608ec7f6a849a54c391f515ec77db9b
Stanislav Khromazhenkov <38785016+BloamBla@users.noreply.github.com>
2018-07-12 15:13:27 +0300

    Add files via upload

---
+++ mainJS.js
@@ -0,0 +1,32 @@
+﻿var number = prompt("Введите число больше 0.");
+if (number === null) {
+  alert("Ещё увидимся!");
+}
+else if (number === "") {
+  alert("Вы ничего не ввели!");
+  location.reload();
+}
+else if (number == 0) {
+  alert("Ну я же просил...!");
+  location.reload();
+}
+else if (isNaN(number)) {
+  alert("Это точно не чилсо...");
+  location.reload();
+}
+else {
+  var counter = 0;
+  var simleNumbers = "";
+  function amountOfSimleNumbers(a) {
+nextPrime:
+    for(var i = 2; i <= a; i++) {
+      for (var j = 2; j < i; j++) {
+        if (i % j == 0) continue nextPrime;
+      }
+      simleNumbers += " " + i;
+      counter += 1;
+    }
+    return counter;
+  }
+  alert("Количество простых чисел в интервале от 1 до " + number + " равно - " + amountOfSimleNumbers(number) + ".\n" + "Простые числа в это интервале - " + simleNumbers);
+}
