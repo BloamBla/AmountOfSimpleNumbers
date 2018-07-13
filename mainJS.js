a0aa979c3c0678402411a1294a9c4939101bdef1
Stanislav Khromazhenkov <38785016+BloamBla@users.noreply.github.com>
2018-07-13 17:39:11 +0200

    Update mainJS.js

--- mainJS.js
+++ mainJS.js
@@ -1,32 +1,30 @@
-﻿var number = prompt("Введите число больше 0.");
+/* eslint-disable no-undef,no-unused-vars,max-len,no-inner-declarations */
+let number = prompt('Введите число больше 0.');
 
-function amountOfSimleNumbers(a) {
-    var counter = 0;
-    var simleNumbers = "";
-nextPrime:
-    for(var i = 2; i <= a; i++) {
-      for (var j = 2; j < i; j++) {
-        if (i % j == 0) continue nextPrime;
+if (number === null) {
+  alert('Ещё увидимся!');
+} else if (number === '') {
+  alert('Вы ничего не ввели!');
+  location.reload();
+} else if (number < 1) {
+  alert('Ну я же просил...!');
+  location.reload();
+} else if (isNaN(number)) {
+  alert('Это точно не число...');
+  location.reload();
+} else {
+  let counter = 0;
+  let simpleNumbers = '';
+  function amountOfSimpleNumbers(a) {
+    nextPrime:
+    for (let i = 2; i <= a; i++) {
+      for (let j = 2; j < i; j++) {
+        if (i % j === 0) continue nextPrime;
       }
-      simleNumbers += " " + i;
+      simpleNumbers += ' ' + i;
       counter += 1;
     }
     return counter;
   }
-if (number === null) {
-  alert("Ещё увидимся!");
-}
-else if (number === "") {
-  alert("Вы ничего не ввели!");
-  location.reload();
-} else if (number < 1) {
-  alert("Ну я же просил...!");
-  location.reload();
-}
-else if (isNaN(number)) {
-  alert("Это точно не число...");
-  location.reload();
-}
-else {
-  alert("Количество простых чисел в интервале от 1 до " + number + " равно - " + amountOfSimleNumbers(number) + ".\n" + "Простые числа в это интервале - " + simleNumbers);
+  alert('Количество простых чисел в интервале от 1 до ' + number + ' равно - ' + amountOfSimpleNumbers(number) + '.\n' + 'Простые числа в это интервале - ' + simpleNumbers);
 }
