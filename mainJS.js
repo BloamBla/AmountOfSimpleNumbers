aed7ad092c1ca0d75b5dcd6a4c00bc6e5de9584c
Mikalai Bury <nikolay.bury@specific-group.com>
2018-07-24 10:48:39 +0300

    fixes, refactoring

--- mainJS.js
+++ mainJS.js
@@ -1,42 +1,49 @@
-/* eslint-disable no-undef,no-unused-vars,max-len,no-inner-declarations */
-let number = prompt('Введите число больше 0.');
+/* eslint-disable no-undef */
+
+function amountOfSimpleNumbers(a) {
+  let isSimpleNumber = false;
+  // let simpleNumbers = '';  can be array
+  let simpleNumbers = [];
+
+  /* if (i === 2) {
+    simpleNumbers += ' ' + i;
+    counter += 1;
+  }*/
+
+  simpleNumbers.push(2);
+
+  for (let i = 3; i <= a; i++) {
+    isSimpleNumber = true;
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isSimpleNumber = false;
+        break;
+      }
+    }
+    // if (isSimpleNumber === true) { can be simplifided to
+    if (isSimpleNumber) {
+      simpleNumbers.push(i);
+    }
+  }
+  return simpleNumbers; // return array. from array we can get count and numbers
+}
+
+
+let number = prompt('Введите число больше 1.');
 
 if (number === null) {
   alert('Ещё увидимся!');
 } else if (number === '') {
   alert('Вы ничего не ввели!');
   location.reload();
-} else if (number < 1) {
+} else if (number < 2) {
   alert('Ну я же просил...!');
   location.reload();
 } else if (isNaN(number)) {
   alert('Это точно не число...');
   location.reload();
 } else {
-  function amountOfSimpleNumbers(a) {
-    let isSimpleNumber = false;
-    let simpleNumbers = '';
-    let counter = 0;
-    for (let i = 2; i <= a; i++) {
-      if (i === 2) {
-        simpleNumbers += ' ' + i;
-        counter += 1;
-      } else {
-        for (let j = 2; j < i; j++) {
-          if (i % j === 0) {
-            isSimpleNumber = false;
-            break;
-          } else isSimpleNumber = true;
-        }
-        if (isSimpleNumber === true) {
-          simpleNumbers += ' ' + i;
-          counter += 1;
-        }
-      }
-    }
-    return [simpleNumbers, counter];
-  }
   let result = amountOfSimpleNumbers(number);
   alert('Количество простых чисел в интервале от 1 до ' + number + ' равно - ' +
-      result[1] + '.\n' + 'Простые числа в это интервале - ' + result[0]);
+    result.length + '.\n' + 'Простые числа в это интервале - ' + result.join(','));
 }
