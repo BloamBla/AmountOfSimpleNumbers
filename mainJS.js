736a9b4a0040f532f0e066290f135072159e255f
Stanislav Khromazhenkov <38785016+BloamBla@users.noreply.github.com>
2018-08-01 21:03:37 +0200

    Update mainJS.js

--- mainJS.js
+++ mainJS.js
@@ -1,49 +1,43 @@
 /* eslint-disable no-undef */
 
-function amountOfSimpleNumbers(a) {
-  let isSimpleNumber = false;
-  // let simpleNumbers = '';  can be array
-  let simpleNumbers = [];
+function validAndAmount(form) {
+  let input = form.inputBox.value;
 
-  /* if (i === 2) {
-    simpleNumbers += ' ' + i;
-    counter += 1;
-  }*/
+  if (input === null) {
+    alert('Ещё увидимся!');
+  } else if (input === '') {
+    alert('Вы ничего не ввели!');
+    location.reload();
+  } else if (input < 2) {
+    alert('Это число меньше, либо равно 1!');
+    location.reload();
+  } else if (isNaN(input)) {
+    alert('Это точно не число...');
+    location.reload();
+  } else {
+    function amountOfSimpleNumbers(a) {
+      let isSimpleNumber = false;
+      let simpleNumbers = [];
 
-  simpleNumbers.push(2);
+      simpleNumbers.push(2);
 
-  for (let i = 3; i <= a; i++) {
-    isSimpleNumber = true;
-    for (let j = 2; j < i; j++) {
-      if (i % j === 0) {
-        isSimpleNumber = false;
-        break;
+      for (let i = 3; i <= a; i++) {
+        isSimpleNumber = true;
+        for (let j = 2; j < i; j++) {
+          if (i % j === 0) {
+            isSimpleNumber = false;
+            break;
+          }
+        }
+        if (isSimpleNumber) {
+          simpleNumbers.push(i);
+        }
       }
+      return simpleNumbers;
     }
-    // if (isSimpleNumber === true) { can be simplifided to
-    if (isSimpleNumber) {
-      simpleNumbers.push(i);
-    }
+
+    let result = amountOfSimpleNumbers(input);
+    document.getElementById('amountOfSimpleNumbers').value = result.length;
+    document.getElementById('simpleNumbers').value = result.join(', ');
   }
-  return simpleNumbers; // return array. from array we can get count and numbers
-}
-
-
-let number = prompt('Введите число больше 1.');
-
-if (number === null) {
-  alert('Ещё увидимся!');
-} else if (number === '') {
-  alert('Вы ничего не ввели!');
-  location.reload();
-} else if (number < 2) {
-  alert('Ну я же просил...!');
-  location.reload();
-} else if (isNaN(number)) {
-  alert('Это точно не число...');
-  location.reload();
-} else {
-  let result = amountOfSimpleNumbers(number);
-  alert('Количество простых чисел в интервале от 1 до ' + number + ' равно - ' +
-    result.length + '.\n' + 'Простые числа в это интервале - ' + result.join(','));
 }
