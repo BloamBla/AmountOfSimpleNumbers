a17c2ba05f6b99b9c4c549e9322e4919a93dea7d
Stanislav Khromazhenkov <38785016+BloamBla@users.noreply.github.com>
2018-08-04 21:30:02 +0200

    Update mainJS.js

--- mainJS.js
+++ mainJS.js
@@ -1,5 +1,28 @@
 /* eslint-disable no-undef */
 
+
+function amountOfSimpleNumbers(a) {
+  let isSimpleNumber = false;
+  let simpleNumbers = [];
+
+  simpleNumbers.push(2);
+
+  for (let i = 3; i <= a; i++) {
+    isSimpleNumber = true;
+
+    for (let j = 2; j < i; j++) {
+      if (i % j === 0) {
+        isSimpleNumber = false;
+        break;
+      }
+    }
+    if (isSimpleNumber) {
+      simpleNumbers.push(i);
+    }
+  }
+  return simpleNumbers;
+}
+
 function validAndAmount(form) {
   let input = form.inputBox.value;
 
@@ -15,29 +38,12 @@ function validAndAmount(form) {
     alert('Это точно не число...');
     location.reload();
   } else {
-    function amountOfSimpleNumbers(a) {
-      let isSimpleNumber = false;
-      let simpleNumbers = [];
-
-      simpleNumbers.push(2);
-
-      for (let i = 3; i <= a; i++) {
-        isSimpleNumber = true;
-        for (let j = 2; j < i; j++) {
-          if (i % j === 0) {
-            isSimpleNumber = false;
-            break;
-          }
-        }
-        if (isSimpleNumber) {
-          simpleNumbers.push(i);
-        }
-      }
-      return simpleNumbers;
-    }
-
     let result = amountOfSimpleNumbers(input);
     document.getElementById('amountOfSimpleNumbers').value = result.length;
     document.getElementById('simpleNumbers').value = result.join(', ');
   }
 }
+
+let submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', validAndAmount);
